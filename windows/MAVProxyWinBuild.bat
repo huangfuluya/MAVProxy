6ed0a7b8fcc8622f5f92592cdc8457ba05cacd14
huangfuluya <869130969@qq.com>
2025-02-23 18:46:29 +0800

    添加了vjoy的配置文件，以后再启动仿真时可以指向这个仓库，直接使用最新版本的mavproxy。修改了winbuild的脚本，但也没有成功编译出来

--- windows/MAVProxyWinBuild.bat
+++ windows/MAVProxyWinBuild.bat
@@ -31,11 +31,11 @@ if exist "..\..\pymavlink" (
 )
 
 rem -----Install additional Python packages-----
-python.exe -m pip install -U wheel setuptools pip
-python.exe -m pip install pywin32 lxml pymavlink numpy matplotlib pyserial opencv-python PyYAML Pygame Pillow wxpython prompt-toolkit scipy
-python.exe -m pip install -U openai wave pyaudio
-python.exe -m pip install -U pyinstaller==6.7.0 packaging 
-python.exe -m pip install -U requests
+python.exe -m pip install -U wheel setuptools pip --user
+python.exe -m pip install pywin32 lxml pymavlink numpy matplotlib pyserial opencv-python PyYAML Pygame Pillow wxpython prompt-toolkit scipy --user
+python.exe -m pip install -U openai wave pyaudio --user
+python.exe -m pip install -U pyinstaller==6.7.0 packaging --user
+python.exe -m pip install -U requests --user
 
 rem -----Build MAVProxy-----
 cd ..\
